a5d8a76e0bde079b3144df7491ffd85511d55100
Abdi <abdullahia0215@gmail.com>
2023-09-24 16:16:15 -0500

    set up the bones for GalleryItem

---
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -0,0 +1,8 @@
+const GalleryItem = ({ item, getGalleryItems }) => { 
+    console.log("in GalleryItem:", item);
+
+    const [isDescriptionVisible, setIsDescriptionVisible] = useState(false);
+
+}
+export default GalleryItem;
+   
