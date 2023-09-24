8c9a0d746e996d917fc3607826424916893bce00
Abdi <abdullahia0215@gmail.com>
2023-09-24 16:25:02 -0500

    added toggleVisibility const and increaseLikes put request

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,8 +1,21 @@
 const GalleryItem = ({ item, getGalleryItems }) => { 
     console.log("in GalleryItem:", item);
 
-    const [isDescriptionVisible, setIsDescriptionVisible] = useState(false);
+    const [bioVisible, setbioVisible] = useState(false);
 
-}
+    const togglebioVisibility = () => {
+        setbioVisible(!bioVisible);
+    }
+    const increaseLikes = (event) => {
+        axios.put(`/gallery/liked/${item.id}`)
+            .then(response => {
+                getGalleryItems();
+            })
+            .catch(error => {
+                console.log(error);
+                console.error('Error updating likes');
+            });
+    }
+};
 export default GalleryItem;
    
